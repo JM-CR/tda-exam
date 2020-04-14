6537fb5ac18b5ff06b2a12a49c58f0d0e49c9272
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-04-14 11:41:08 -0500

    Optimizacion de directorios

---
+++ template/library.c
@@ -0,0 +1,29 @@
+// See library.h for more info
+// Author: Josue Mosiah Contreras Rocha
+// File: library.c
+// Date: 17/02/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include "library.h"
+
+
+// -----------------------------
+// Private elements
+// -----------------------------
+
+/* Private macros and constants */
+
+/* Private types */
+
+/* Private global variables */
+
+/* Private functions */
+
+
+// -----------------------------
+// Public elements
+// -----------------------------
+
+/* Implementation of the public functions */
---
+++ template/library.h
@@ -0,0 +1,31 @@
+// Template to create C libraries.
+// Author: Josue Mosiah Contreras Rocha
+// File: library.h
+// Date: 17/02/20
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+// -----------------------------
+// System headers
+// -----------------------------
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Constants */
+
+/* Types declarations */
+
+/* Global variables */
+
+/* Function prototypes */
+
+/**
+ * Function's documentation.
+ */
+
+
+#endif
