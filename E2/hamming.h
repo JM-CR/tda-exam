025f28c7a0cfd90692b22dde98843ac6926aad41
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-04-22 15:31:55 -0500

    Initial template for the exam

---
+++ E2/hamming.h
@@ -0,0 +1,31 @@
+// This library simulates the hamming codes.
+// Author: Josue Mosiah Contreras Rocha
+// File: hamming.h
+// Date: 23/02/20
+
+#ifndef HAMMING_H
+#define HAMMING_H
+
+// -----------------------------
+// System headers
+// -----------------------------
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Constants */
+
+/* Types declarations */
+
+/* Global variables */
+
+/* Function prototypes */
+
+/**
+ * Function's documentation.
+ */
+
+
+#endif
---
+++ E2/main.c
@@ -0,0 +1,13 @@
+// Author: Josue Mosiah Contreras Rocha
+// File: main.c
+// Date: 23/04/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include "hamming.h"
+
+int main( int argc, char *argv[] ) {
+
+    return 0;
+}
